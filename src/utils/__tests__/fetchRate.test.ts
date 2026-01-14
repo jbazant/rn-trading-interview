10f49b152fc45cec367e34e09a0aa05c93f88089
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-01-14 17:52:07 +0100

    Allow to simulate bad network connection

--- src/utils/__tests__/fetchRate.test.ts
+++ src/utils/__tests__/fetchRate.test.ts
@@ -1,9 +1,9 @@
-import { fetchRate } from '../fetchRate';
+import { fetchRate, simulateBadNetwork } from '../fetchRate';
 
 describe('fetchRate', () => {
     let fetchMock: jest.SpyInstance;
 
-    beforeAll(() => {
+    beforeEach(() => {
         fetchMock = jest.spyOn(global, 'fetch').mockResolvedValue({
             json: async () => ({ data: { amount: '95956.01', base: 'BTC', currency: 'USD' } }),
             ok: true,
@@ -11,10 +11,26 @@ describe('fetchRate', () => {
         } as Response);
     });
 
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
     it('should fetch rate for given asset', async () => {
         const result = await fetchRate('BTC');
 
         expect(result).toBe('95956.01');
         expect(fetchMock).toHaveBeenCalledWith('https://api.coinbase.com/v2/prices/BTC-USD/buy');
+    });
+
+    describe('simulateBadNetwork', () => {
+        it('should throw an error when network error is simulated', async () => {
+            // Mock Math.random to always return a value that triggers the error
+            jest.spyOn(Math, 'random').mockReturnValue(0.3); // 10% < 20% error rate
+
+            await expect(() => simulateBadNetwork(0, 0.5)).rejects.toThrow(
+                'Network error while fetching rate',
+            );
+            await expect(simulateBadNetwork(1, 0.1)).resolves.toBeNull();
+        });
     });
 });
--- src/utils/fetchRate.ts
+++ src/utils/fetchRate.ts
@@ -1,10 +1,24 @@
 const RATE_URL = 'https://api.coinbase.com/v2/prices/{asset}-USD/buy';
 
+const NETWORK_ERROR_RATE = 0; // chance to simulate a network error
+const NETWORK_DELAY = 1e3; // delay to simulate network latency
+
 const getRateUrl = (asset: string) => {
     return RATE_URL.replace('{asset}', asset);
 };
 
+export const simulateBadNetwork = (networkDelay: number, errorRate: number) =>
+    new Promise((resolve, reject) =>
+        setTimeout(() => {
+            if (Math.random() < errorRate) {
+                return reject(new Error(`Network error while fetching rate`));
+            }
+            resolve(null);
+        }, networkDelay),
+    );
+
 export const fetchRate = async (asset: string): Promise<string> => {
+    await simulateBadNetwork(NETWORK_DELAY, NETWORK_ERROR_RATE);
     const response = await fetch(getRateUrl(asset));
     const data = await response.json();
     return data.data.amount;
